fd809fc774a2dadecaf0f4ac42d61ef25366c4da
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-07-31 18:17:30 -0500

    feat: se crearon datos fake en la base de datos

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 let formulario = document.getElementById("cliente-form")
 let espacioDondeSeMuestraLosClientes = document.querySelector(".tbody-clientes")
 
+document.addEventListener("DOMContentLoaded", function () {
+    // aca va ir a la base de datos y me a traer a todos los clientes y luego los va a pintar
+})
+
+
+
+
+
+formulario.addEventListener("click", function () {
+    alert("le diste click al formulario")
+})
